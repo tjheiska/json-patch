79c49a48b7ee9efdf15c6f7c3465a4c407436f6b
tjheiska <tjheiska@tjheiska-XPS-15-9570>
2018-10-16 08:19:55 +0300

    Implementation of test-operation

--- src/main/java/com/tananaev/jsonpatch/gson/AbsOperationDeserializer.java
+++ src/main/java/com/tananaev/jsonpatch/gson/AbsOperationDeserializer.java
@@ -16,25 +16,41 @@
 
 package com.tananaev.jsonpatch.gson;
 
-import com.google.gson.*;
-import com.tananaev.jsonpatch.operation.*;
-
 import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+import com.tananaev.jsonpatch.operation.AbsOperation;
+import com.tananaev.jsonpatch.operation.AddOperation;
+import com.tananaev.jsonpatch.operation.MoveOperation;
+import com.tananaev.jsonpatch.operation.RemoveOperation;
+import com.tananaev.jsonpatch.operation.ReplaceOperation;
+import com.tananaev.jsonpatch.operation.TestOperation;
 
 public class AbsOperationDeserializer implements JsonDeserializer<AbsOperation> {
 
-    @Override
-    public AbsOperation deserialize(JsonElement element, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        String operation = element.getAsJsonObject().getAsJsonPrimitive("op").getAsString();
-        return jsonDeserializationContext.deserialize(element, getType(operation));
-    }
+	@Override
+	public AbsOperation deserialize(JsonElement element, Type type,
+			JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+		String operation = element.getAsJsonObject().getAsJsonPrimitive("op").getAsString();
+		return jsonDeserializationContext.deserialize(element, getType(operation));
+	}
 
-    private Type getType(String operation) throws JsonSyntaxException {
-        if ("add".equals(operation)) return AddOperation.class;
-        if ("move".equals(operation)) return MoveOperation.class;
-        if ("remove".equals(operation)) return RemoveOperation.class;
-        if ("replace".equals(operation)) return ReplaceOperation.class;
-        throw new JsonSyntaxException("operation " + operation + " not supported");
-    }
+	private Type getType(String operation) throws JsonSyntaxException {
+		if ("add".equals(operation))
+			return AddOperation.class;
+		if ("move".equals(operation))
+			return MoveOperation.class;
+		if ("remove".equals(operation))
+			return RemoveOperation.class;
+		if ("replace".equals(operation))
+			return ReplaceOperation.class;
+		if ("test".equals(operation))
+			return TestOperation.class;
+		throw new JsonSyntaxException("operation " + operation + " not supported");
+	}
 
 }
---
+++ src/main/java/com/tananaev/jsonpatch/operation/TestOperation.java
@@ -0,0 +1,38 @@
+package com.tananaev.jsonpatch.operation;
+
+import java.util.Objects;
+
+import com.google.gson.JsonElement;
+import com.google.gson.annotations.SerializedName;
+import com.tananaev.jsonpatch.JsonPath;
+
+public class TestOperation extends AbsOperation {
+
+	@SerializedName("value")
+	public JsonElement data;
+
+	public TestOperation(JsonPath path, JsonElement data) {
+		this.data = data;
+		this.path = path;
+	}
+
+	@Override
+	public String getOperationName() {
+		return "test";
+	}
+
+	@Override
+	public JsonElement apply(JsonElement original) {
+
+		JsonElement result = duplicate(original);
+
+		JsonElement item = path.navigate(result);
+
+		if (Objects.equals(item, data)) {
+			return result;
+		}
+
+		return null;
+	}
+
+}
